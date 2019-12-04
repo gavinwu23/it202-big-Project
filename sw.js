11af5ac5c0439f33251bac2ee2cdc174395c1415
Gavin <gwu23@uic.edu>
2019-12-04 10:38:52 -0600

    fixed stuff

--- sw.js
+++ sw.js
@@ -1,27 +1,27 @@
-importScripts('/it202-project2/cache-polyfill.js');
+importScripts('/it202-big-Project/cache-polyfill.js');
 
 self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('mmovie').then(function(cache) {
      return cache.addAll([
-       '/it202-bigProject',
-       '/it202-bigProject/index.html',
-       "/it202-bigProject/page_content/about.html",
-       "/it202-bigProject/page_content/home.html",
-       "/it202-bigProject/page_content/library.html",
-       "/it202-bigProject/page_content/picture.html",
-       "/it202-bigProject/page_content/weather.html",
-       "/it202-bigProject/images/beijing.jpeg",
-       "/it202-bigProject/images/camera.jpg",
-       "/it202-bigProject/images/city.jpeg",
-       "/it202-bigProject/images/dubai.jpg",
-       "/it202-bigProject/images/library.jpg",
-       "/it202-bigProject/images/library2.jpg",
-       "/it202-bigProject/images/losAngles.jpg",
-       "/it202-bigProject/images/paris.jpeg",
-       "/it202-bigProject/images/sun.jpg",
-       "/it202-bigProject/images/weather.jpg",
-       "/it202-bigProject/images/ww.png"
+       '/it202-big-Project',
+       '/it202-big-Project/index.html',
+       "/it202-big-Project/page_content/about.html",
+       "/it202-big-Project/page_content/home.html",
+       "/it202-big-Project/page_content/library.html",
+       "/it202-big-Project/page_content/picture.html",
+       "/it202-big-Project/page_content/weather.html",
+       "/it202-big-Project/images/beijing.jpeg",
+       "/it202-big-Project/images/camera.jpg",
+       "/it202-big-Project/images/city.jpeg",
+       "/it202-big-Project/images/dubai.jpg",
+       "/it202-big-Project/images/library.jpg",
+       "/it202-big-Project/images/library2.jpg",
+       "/it202-big-Project/images/losAngles.jpg",
+       "/it202-big-Project/images/paris.jpeg",
+       "/it202-big-Project/images/sun.jpg",
+       "/it202-big-Project/images/weather.jpg",
+       "/it202-big-Project/images/ww.png"
      ]);
    })
  );
